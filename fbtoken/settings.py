94fb439d1983e4b65cb5c66a644bbe62cfac53b8
Brian Elliott <bdelliott@gmail.com>
2012-07-30 12:53:50 -0500

    Further progress.
    * working login
    * partial jquery config
    * needs code to post token back to server

--- fbtoken/settings.py
+++ fbtoken/settings.py
@@ -26,6 +26,9 @@ def get_database_config():
     items = [(k.upper(),v) for (k,v)  in items]
     return {'default': dict(items)}
 
+# Base absolute url:
+BASE_URL = config.get("DEFAULT", "base_url")
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -76,17 +79,18 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(get_base_dir(), "staticfiles")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = BASE_URL+'/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(get_project_dir(), "static"),
 )
 
 # List of finder classes that know how to find static files in
@@ -200,9 +204,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'tokenmanager.context_processors.fb_settings',
     'tokenmanager.context_processors.base_url',
 )
-
-# Base absolute url:
-BASE_URL = config.get("DEFAULT", "base_url")
 
 # Facebook app settings
 FACEBOOK_APP_ID = config.get("facebook", "app_id")
